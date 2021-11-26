9f76147cc88a26b1f8b95e5b9b78ce87b6608241
Maratov Maksat <82010556+madmaaax@users.noreply.github.com>
2021-11-26 16:35:09 +0600

    asd
    asd

---
+++ SDProject/src/com/company/facadePattern/ServiceFacade.java
@@ -0,0 +1,31 @@
+package com.company.facadePattern;
+
+public class ServiceFacade {
+    public void serveClient(){
+        // hello client
+        // give menu
+        // take order
+        // calculate and give cost
+        // calculate and give info about time to wait
+        // end
+        System.out.println("");
+    }
+
+    public void takeOrder(){
+        // take order info(id, what dishes to cook)
+        // cook
+        // give to waiter
+    }
+
+    public void serveClientSecond(){
+        // take dishes
+        // give dishes
+        // take money
+    }
+
+    public void cleanerWork(){
+
+    }
+
+
+}
