3ae99fd35b3a5f8343e59813e4af36f0432cbc8a
Koffer0-0 <68071028+Koffer0-0@users.noreply.github.com>
2021-11-26 15:20:35 +0600

    добавляю менюшку

--- SDProject/src/com/company/Main.java
+++ SDProject/src/com/company/Main.java
@@ -1,20 +1,46 @@
 package com.company;
 
+import java.nio.channels.ScatteringByteChannel;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        //кароче здесь всякую херню не создавать, потом будем здесь вызывать only(fans) классы и методы ничего болльшего
-        //типо чтобы не заполнять
         Scanner input = new Scanner(System.in);
-//        Troop troop = null;
-//        Barracks barracks = null;
-//
-//        while (true) {
-//            int choose = menu(input);
-//            selector(choose, troop, barracks);
-//        }
+
+        while (true) {
+            int choose = menu(input);
+            selector(choose);
+        }
     }
 
+    public static short menu(Scanner input) {
+        short option;
+        System.out.println("\nДобро пожаловать в СТОЛОВКУ, что желаете заказать?");
+        System.out.println("1. Гуляш");
+        System.out.println("2. Манты");
+        System.out.println("3. Самсу");
+        option = input.nextByte();
+        return option;
+    }
+
+    public static void selector(int choose) {
+        switch (choose) {
+            case 1:
+//                troop = new Knight();
+//                barracks = new KnightBarracks();
+                break;
+            case 2:
+//                troop = new Marksman();
+//                barracks = new MarksmanBarracks();
+                break;
+            case 3:
+//                troop = new Wizzard();
+//                barracks = new WizzardBarracks();
+                break;
+            default:
+                System.out.println("ОШИБКА! Выберите вариант от 1 до 3!");
+                break;
+        }
+    }
 }
